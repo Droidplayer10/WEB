3ec21084e7aa4fec2ecd54036dba7157c566a755
Droidplayer10 <91594406+Droidplayer10@users.noreply.github.com>
2022-05-13 00:05:11 +0200

    Add files via upload

---
+++ Hazi-arfolyam/alap.js
@@ -0,0 +1,34 @@
+fetch("https://api.exchangerate-api.com/v4/latest/USD")
+.then(x => x.json())
+.then(y => megjelenit(y));
+
+function megjelenit(adatok){
+    console.log(adatok)
+    tomb=adatok;
+    var sz = '';
+    for (var elem of adatok.rates) {
+       sz+='<div class="col-sm-3">'
+       sz+='<p>'+elem+'</p>'
+       sz+='</div>'
+
+        
+    }
+    document.getElementById("#usd_huf").innerHTML=sz;
+    
+
+
+
+}
+
+
+
+/*
+1. Írasd ki 1 usa dollár hány forint: HUF, lapon felül jelenjen meg!
+
+2. Készíts beviteli mezőt, ha beírom, hogy usd akkor írja ki hány forint.
+De ha beírom pl. EUR, akkor az írja ki 1 euró hány forint!
+3. Bootstrapet használj, szép legyen, intelligens, kiírásokkal!!!!!!
+4. Tedd fel saját githubodra, és ide a linkjét küld el a lapnak!!!!!
+Tanárnő.
+
+*/
